4de3da83614c94000678a35d25d0af8b8692d5bf
HUYNHKHANH2006 <khanhhuynhtrai@gmail.com>
2026-04-24 10:59:07 +0700

    Create bai2.java

---
+++ day3/bai2.java
@@ -0,0 +1,15 @@
+package day3;
+
+import java.util.*;
+
+public class bai2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(5, 2, 8)); // Nhập
+        list.add(9);                       // Thêm
+        list.set(0, 10);                   // Sửa (tại index 0)
+        list.remove(Integer.valueOf(2));   // Xóa (số 2)
+        System.out.println("Vị trí số 8: " + list.indexOf(8)); // Tìm
+        Collections.sort(list);            // Sắp xếp
+        System.out.println("Đã sắp xếp: " + list);
+    }
+}
