174559d953ddfea5b80dd75552f3d7f9420d3efc
HUYNHKHANH2006 <khanhhuynhtrai@gmail.com>
2026-04-25 09:19:33 +0700

    Create bt1arraylist.java

---
+++ day4/bt1arraylist.java
@@ -0,0 +1,13 @@
+/*
+Bài tập 1 : Viết chương trình quản lý Tên Sinh Viên dưới dạng
+console, yêu cầu sử dụng ArrayList và thực hiện các chức năng
+sau (tạo menu):
+a) Thêm Sinh viên
+b) Xuất danh sách sinh viên
+c) Sửa Sinh Viên
+d) Xóa Sinh viên chứa tên bất kỳ
+e) Tìm Sinh viên mà tên có chữ An
+f) Sắp xếp Sinh Viên
+g) Xuất ra số lượng sinh viên
+làm như menu để người dùng chọn chức năng muốn thực hiện, sau đó thực hiện chức năng đó. và có nút thoát chương trình.
+*/
