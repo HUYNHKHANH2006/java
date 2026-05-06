f5a04434ad2d475b85a082cc952d0a676f8d9455
HUYNHKHANH2006 <khanhhuynhtrai@gmail.com>
2026-05-06 11:29:23 +0700

    Create hamlonhon.java

---
+++ day5/hamlonhon.java
@@ -0,0 +1,32 @@
+/* Ham lon hon  dùng oop
+
+Số sản phẩm                Đơn giá
+
+1-199                       0.5
+
+200 - 399               0.55
+
+400 - 599               0.6
+
+600 trở lên                0.65 
+
+
+
+
+
+
+
+
+
+
+Viết hàm LonHon(Nhan Vien nv2): hàm này trả về giá trị true khi số sản phẩm
+(SoSP) lớn hơn số sản phẩm của nv2, ngược lại trả về false
+>Viết hàm main sử dụng lớp Nhan Vien theo yêu cầu sau:
+Cho người dùng nhập vào 2 nhân viên, mỗi nhân viên nhập vào họ, tên, số
+sản phẩm của họ. Hãy tính và xuất ra lương của từng nhân viên.
+*/
+package day5;
+
+public class hamlonhon {
+    
+}
